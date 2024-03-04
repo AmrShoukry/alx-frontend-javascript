6bf77ef0503529b7b53161a6c60d3e987bff0a3f
Amr Shoukry <shoukryworkamr1@gmail.com>
2024-03-04 23:39:29 +0200

    task10

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,7 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const newArr = [];
+  for (const element of array) {
+    newArr.push(appendString + element);
+  }
+  return newArr;
+}
