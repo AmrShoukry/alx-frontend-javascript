f382ad78252edae78834834fd661f637bd0f5a14
Amr Shoukry <shoukryworkamr1@gmail.com>
2024-03-08 15:21:37 +0200

    task3 fix2

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   const promise1 = uploadPhoto();
   const promise2 = createUser();
-  return Promise.all(promise1, promise2)
+  return Promise.all([promise1, promise2])
     .then((responses) => {
       console.log(
         `${responses[0].body} ${responses[1].firstName} ${responses[1].lastName}`,
