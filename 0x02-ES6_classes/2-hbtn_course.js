537e7701629aa56373efdb5f86757be2fafb113f
Amr Shoukry <shoukryworkamr1@gmail.com>
2024-03-08 16:59:24 +0200

    task2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,51 @@
+function checkVariable(variable, text, type) {
+  if (variable === undefined || typeof variable !== type) {
+    throw new TypeError(`${text} must be a ${type}`);
+  }
+  if (type === 'object' && Array.isArray(variable)) {
+    for (const student of variable) {
+      if (typeof student !== 'string') {
+        throw new TypeError('Each student must be a string');
+      }
+    }
+  }
+}
+
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    checkVariable(name, 'Name', 'string');
+    checkVariable(length, 'Length', 'number');
+    checkVariable(students, 'Students', 'object');
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set name(value) {
+    checkVariable(value, 'Name', 'string');
+    this._name = value;
+  }
+
+  set length(value) {
+    checkVariable(value, 'Length', 'number');
+    this._length = value;
+  }
+
+  set students(value) {
+    checkVariable(value, 'Students', 'object');
+    this._students = value;
+  }
+}
