bdb9327897682e14f47e4e764233db9a9d6bf69e
Amr Shoukry <shoukryworkamr1@gmail.com>
2024-03-08 15:07:05 +0200

    task2 fix2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-unused-vars */
 export default function handleResponseFromAPI(promise) {
   promise
-    .then((response) => ({
-      status: 200,
-      body: 'success',
-    }))
-    .catch((err) => new Error())
-    .finally(() => {
+    .then((response) => {
       console.log('Got a response from the API');
-    });
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    .catch((err) => new Error());
 }
