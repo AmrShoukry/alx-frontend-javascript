c1edafdb364e3a1a40306d4bb964b4b9f889b336
Amr Shoukry <shoukryworkamr1@gmail.com>
2024-03-08 15:36:46 +0200

    task6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const promise1 = signUpUser(firstName, lastName);
+  const promise2 = uploadPhoto(fileName);
+  return Promise.allSettled([promise1, promise2])
+    .then((responses) => [
+      {
+        status: responses[0].status,
+        value: responses[0].value,
+      },
+      {
+        status: responses[1].status,
+        value: responses[1].value,
+      },
+    ])
+    .catch((err) => {});
+}
