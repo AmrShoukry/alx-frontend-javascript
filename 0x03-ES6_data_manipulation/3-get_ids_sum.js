bfe07f28a1eb8ba56cd63b9272b79703f3bb176c
Amr Shoukry <shoukryworkamr1@gmail.com>
2024-03-11 17:46:20 +0200

    task3 fix

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,3 @@
 export default function getStudentIdsSum(students) {
-  return students.reduce((sum, el) => {
-    return sum + el.id;
-  }, 0);
+  return students.reduce((sum, el) => sum + el.id, 0);
 }
